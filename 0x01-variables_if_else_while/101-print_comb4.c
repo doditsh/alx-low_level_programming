f51eae303826cf70231e9121ddad31e800a743ae
doditsh <doditshionza@gmail.com>
2022-09-12 15:28:37 -0400

    Assigment

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Prints all possible combinations of three different digits,
+ *main -  Prints all possible combinations of three different digits,
  *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
