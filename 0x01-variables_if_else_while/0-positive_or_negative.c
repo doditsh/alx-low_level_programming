a5d9ac4af925b9ef3c5d043a2bcba44306fa9d03
doditsh <doditshionza@gmail.com>
2022-09-12 10:43:43 -0400

    Assigment

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,3 @@
-*
- * File: 0-positive_or_negative.c
- * Author: Tshisekedi dodi Tshionza
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -12,8 +7,7 @@
  *        it is positive, negative, or zero.
  *
  * Return: 0.  
- * To show that the code ran successfully 
-*/
+ */
 int main(void)
 {
 	int n;
