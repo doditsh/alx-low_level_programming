b5e03f3dd0e80c4a6278748d0f881b53f05eeb74
doditsh <doditshionza@gmail.com>
2022-09-15 15:55:19 -0400

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/* main - Write a program that prints _putchar, followed by a new line.
+ * return 0 upon success
+ *
+ */
+
+int main (void){
+        putchar('p');
+	putchar('u');
+        putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+
+
+
+
+return (0);
+}
+
