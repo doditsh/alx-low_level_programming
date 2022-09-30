337fac5a86388fd35042399b98654c9ea491bc97
doditsh <doditshionza@gmail.com>
2022-09-29 22:10:06 -0400

    0x08-recusion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,25 +2,15 @@
 
 /**
  * _puts_recursion - prints a string, followed by a new line
- *  
- *  Return 0 upon success
+ * @s: string to print
  */
-
-
 void _puts_recursion(char *s)
 {
-       
-        
-       if (*s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
-
-
-	
-
-
 }
